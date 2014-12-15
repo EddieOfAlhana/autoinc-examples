be60bac3cb8f0a9ec650599b4fab1fce46024776
unknown <Viktoria_Vinkovics@EPHUBUDW2090.budapest.epam.com>
2014-12-15 16:57:22 +0100

    add vinkovicsviki.java

---
+++ vinkovicsviki.java
@@ -0,0 +1,34 @@
+/*
+Write a program to convert binary format to decimal number using numeric operations.
+     
+Examples: 
+	1) 
+	input: 0 
+	output: 0 
+	2) 
+	input: 11 
+	output: 3 
+	3) 
+	input: 110
+	output: 6 
+	4) 
+	input: 100110 
+	output: 1023
+     */
+    
+    public int getDecimalFromBinary(int binary) {
+
+        int decimal = 0;// lock
+        int power = 0;
+        while (true) {
+            if (binary == 0) {
+                break;
+            } else {
+                int tmp = binary % 10;
+                decimal += tmp * Math.pow(2, power);// lock
+                binary = binary / 10;
+                power++;
+            }
+        }
+        return decimal;
+    }
