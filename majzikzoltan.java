f537c7d07e58c696e88396b114f217beb8ae580e
Zoltán Siegl <eddieofalhana@gmail.com>
2014-12-15 17:50:35 +0100

    Update majzikzoltan.java

--- majzikzoltan.java
+++ majzikzoltan.java
@@ -1,12 +1,25 @@
-/*
-     * Description Write a program to find the sum of each digit in the given number.
+    /*
+     * Task: 
+     * Find the sum of digits recusively
+     *
+     * Description:
+     * Write a recursive program to find the sum of each digit in the given number.
      * 
-     * Examples: 1) Input: 9; Result: 9; 2) Input: -13; Result: 4;
-     * 
-     * 3) Input: 12; Result: 3; 4) Input: 12345 Result: 15 4) Input: -12345 Result: 15
+     * Interface to be implemented:
+     * public static Integer sumOfDigits(final int number)
+     *
+     * Examples:
+     * 1) Input: 9; 
+     *    Result: 9; 
+     * 2) Input: -13; 
+     *    Result: 4;
+     * 3) Input: 12; 
+     *    Result: 3; 
+     * 4) Input: 12345 
+     *    Result: 15 
+     * 5) Input: -12345 
+     *    Result: 15
      */
-
-    // This is a one-liner, but in this format it's hard enough...
     public static Integer sumOfDigits(final int number) {
         return //
         (number != 0) //
@@ -19,11 +32,26 @@
     }
 
     /*
-     * Description Write a program to find the sum of each digit in the given number.
+     * Task: 
+     * Find the sum of digits non-recusively
+     *
+     * Description:
+     * Write a non-recursive program to find the sum of each digit in the given number.
      * 
-     * Examples: 1) Input: 9; Result: 9; 2) Input: -13; Result: 4;
-     * 
-     * 3) Input: 12; Result: 3; 4) Input: 12345 Result: 15 4) Input: -12345 Result: 15
+     * Interface to be implemented:
+     * public static Integer sumOfDigitsNonRecursive(int number)
+     *
+     * Examples:
+     * 1) Input: 9; 
+     *    Result: 9; 
+     * 2) Input: -13; 
+     *    Result: 4;
+     * 3) Input: 12; 
+     *    Result: 3; 
+     * 4) Input: 12345 
+     *    Result: 15 
+     * 5) Input: -12345 
+     *    Result: 15
      */
 
     public static Integer sumOfDigitsNonRecursive(int number) {
@@ -35,202 +63,3 @@
         }
         return sum;
     }
-
-
-
-
-
-
-
-//======================
-//=========TEST=========
-//======================
-//
-//This is the full test class with class declarations.
-
-
-
-package com.epam.autioint.sumOfDigits;
-
-import static org.testng.Assert.*;
-
-import org.testng.annotations.Test;
-
-public class SumOfDigitsTest {
-
-    @Test
-    public void testSumOfDigitsShouldReturn1On1() {
-        // Given
-        int number = 1;
-        // When
-        Integer result = SumOfDigits.sumOfDigits(number);
-        // Then
-        Integer expected = 1;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsShouldReturn1On10() {
-        // Given
-        int number = 10;
-        // When
-        Integer result = SumOfDigits.sumOfDigits(number);
-        // Then
-        Integer expected = 1;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsShouldReturn2On11() {
-        // Given
-        int number = 11;
-        // When
-        Integer result = SumOfDigits.sumOfDigits(number);
-        // Then
-        Integer expected = 2;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsShouldReturn18On99() {
-        // Given
-        int number = 99;
-        // When
-        Integer result = SumOfDigits.sumOfDigits(number);
-        // Then
-        Integer expected = 18;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsShouldReturn18On9009() {
-        // Given
-        int number = 9009;
-        // When
-        Integer result = SumOfDigits.sumOfDigits(number);
-        // Then
-        Integer expected = 18;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsShouldReturn21On123456() {
-        // Given
-        int number = 123456;
-        // When
-        Integer result = SumOfDigits.sumOfDigits(number);
-        // Then
-        Integer expected = 21;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsShouldReturnMinus1OnMinus1() {
-        // Given
-        int number = -1;
-        // When
-        Integer result = SumOfDigits.sumOfDigits(number);
-        // Then
-        Integer expected = 1;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsShouldReturnMinus5OnMinus122() {
-        // Given
-        int number = -122;
-        // When
-        Integer result = SumOfDigits.sumOfDigits(number);
-        // Then
-        Integer expected = 5;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsNonRecursiveShouldReturn1On1() {
-        // Given
-        int number = 1;
-        // When
-        Integer result = SumOfDigits.sumOfDigitsNonRecursive(number);
-        // Then
-        Integer expected = 1;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsNonRecursiveShouldReturn1On10() {
-        // Given
-        int number = 10;
-        // When
-        Integer result = SumOfDigits.sumOfDigitsNonRecursive(number);
-        // Then
-        Integer expected = 1;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsNonRecursiveShouldReturn2On11() {
-        // Given
-        int number = 11;
-        // When
-        Integer result = SumOfDigits.sumOfDigitsNonRecursive(number);
-        // Then
-        Integer expected = 2;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsNonRecursiveShouldReturn18On99() {
-        // Given
-        int number = 99;
-        // When
-        Integer result = SumOfDigits.sumOfDigitsNonRecursive(number);
-        // Then
-        Integer expected = 18;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsNonRecursiveShouldReturn18On9009() {
-        // Given
-        int number = 9009;
-        // When
-        Integer result = SumOfDigits.sumOfDigitsNonRecursive(number);
-        // Then
-        Integer expected = 18;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsNonRecursiveShouldReturn21On123456() {
-        // Given
-        int number = 123456;
-        // When
-        Integer result = SumOfDigits.sumOfDigitsNonRecursive(number);
-        // Then
-        Integer expected = 21;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsNonRecursiveShouldReturnMinus1OnMinus1() {
-        // Given
-        int number = -1;
-        // When
-        Integer result = SumOfDigits.sumOfDigitsNonRecursive(number);
-        // Then
-        Integer expected = 1;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-
-    @Test
-    public void testSumOfDigitsNonRecursiveShouldReturnMinus5OnMinus122() {
-        // Given
-        int number = -122;
-        // When
-        Integer result = SumOfDigits.sumOfDigitsNonRecursive(number);
-        // Then
-        Integer expected = 5;
-        assertEquals(result, expected, "SumOfDigits should return " + expected + " when number is " + number);
-    }
-}
