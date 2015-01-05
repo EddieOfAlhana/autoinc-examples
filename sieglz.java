0f13ba363917b4d5cb8cd846a513302d151e1708
Zoltán Siegl <eddieofalhana@gmail.com>
2015-01-05 14:52:59 +0100

    Update sieglz.java

--- sieglz.java
+++ sieglz.java
@@ -1,26 +1,38 @@
 /*
-100 doors in a row are all initially closed. You make
-100 passes by the doors. The first time through, you
+Task: 
+Toggle doors challenge
+
+Description:
+n doors in a row are all initially closed. You make
+n passes by the doors. The first time through, you
 visit every door and toggle the door (if the door is
 closed, you open it; if it is open, you close it).
 The second time you only visit every 2nd door (door
 #2, #4, #6, ...). The third time, every 3rd door
 (door #3, #6, #9, ...), etc, until you only visit
-the 100th door.
+the nth door.
 
 Question: What state are the doors in after the last
 pass? Which are open, which are closed?
 
+Interface to be implemented:
+public Boolean[] toggleDoors(Integer numberOfDoors) 
+
+Examples:
+1) input: 1 result: (true)
+2) input: 2 result: (true, false)
+3) input: 4 result: (true, false, false, true)
+4) input: 9 result: (true, false, false, true, false, false, false, false, true)
 */
 
-public static Boolean[] answer() {
-    Boolean[] result = new Boolean[100];
-    for (Integer i = 0; i < 100; ++i) {
+public Boolean[] toggleDoors(Integer numberOfDoors) {
+    Boolean[] result = new Boolean[numberOfDoors];
+    for (Integer i = 0; i < numberOfDoors; ++i) {
         result[i] = false;
     }
 
-    for (Integer i = 1; i <= 100; ++i) {
-        for (Integer j = i; j <= 100; j = j + i) {
+    for (Integer i = 1; i <= numberOfDoors; ++i) {
+        for (Integer j = i; j <= numberOfDoors; j = j + i) {
             result[j-1] = !result[j-1];
         }
     }
