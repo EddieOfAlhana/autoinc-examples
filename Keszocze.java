d9bf5b30ba83daef8074714536d2d1b1c6f71939
Zoltán Siegl <eddieofalhana@gmail.com>
2015-01-05 14:19:14 +0100

    Update Keszocze.java

--- Keszocze.java
+++ Keszocze.java
@@ -2,27 +2,30 @@
 Given an array of integers as a parameter. 
 Find the 2nd highest value in the array. 
 
-Examples:
-    [6, -1, 3]    -> 3    
-    [2, 2, 1]     -> 1  
-    [-1]          -> null 	
-    [1, 1, 1]     -> null
+Interface to be implemented:
+public Integer secondMax(int[] array)
 
+Examples:
+1) input: [6, -1, 3] result: 3    
+2) input: [2, 2, 1] result: 1  
+3) input: [-1] result: null 	
+4) input: [1, 1, 1]  result: null
 
 */
-	public Integer secondMax(int[] array) { //LOCKED
-		Integer max = null;
-		Integer max2 = null;
-		if (array != null && array.length > 1) {
-			for (int current : array) { //LOCKED
-				if (max == null || max < current) {
-					max2 = max;
-					max = current;
-				} else if ((max2 == null || max2 < current) && current != max) {
-					max2 = current;
-				}
 
+public Integer secondMax(int[] array) { //LOCKED
+	Integer max = null;
+	Integer max2 = null;
+	if (array != null && array.length > 1) {
+		for (int current : array) { //LOCKED
+			if (max == null || max < current) {
+				max2 = max;
+				max = current;
+			} else if ((max2 == null || max2 < current) && current != max) {
+				max2 = current;
 			}
+
 		}
-		return max2;
 	}
+	return max2;
+}
