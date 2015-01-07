615f3034eb15952c97234d7b24951093025808ba
Keszocze Laszlo <Laszlo_Keszocze@epam.com>
2015-01-07 11:51:24 +0100

    New example
    The old duplicate example is replaced by a new one.

--- Keszocze.java
+++ Keszocze.java
@@ -1,28 +1,41 @@
 /*
-Given an array of integers as a parameter. 
-Find the 2nd highest value in the array. 
+Find the line with maximum number of word count in the given file.
+In case if it has multiple lines with max number of words, then it has to list all those lines.
 
 Examples:
-    [6, -1, 3]    -> 3    
-    [2, 2, 1]     -> 1  
-    [-1]          -> null 	
-    [1, 1, 1]     -> null
+But since some light comes through the closed eyelids, a still greater degree 
+of relaxation can be obtained, in all but a few exceptional cases, by 
+excluding it. This is done by covering the closed eyes with the palms of the 
+hands (the fingers being crossed upon the forehead) in such a way as to avoid 
+pressure on the eyeballs. So efficacious is this practice, which I have called 
+“palming,” as a means of relieving strain, that we all instinctively resort to 
+it at times, and from it most patients are able to get a considerable degree 
+of relaxation.
+
+Lines with max word count:
+excluding it. This is done by covering the closed eyes with the palms of the 
+hands (the fingers being crossed upon the forehead) in such a way as to avoid 
+it at times, and from it most patients are able to get a considerable degree
 
 
 */
-	public Integer secondMax(int[] array) { //LOCKED
-		Integer max = null;
-		Integer max2 = null;
-		if (array != null && array.length > 1) {
-			for (int current : array) { //LOCKED
-				if (max == null || max < current) {
-					max2 = max;
-					max = current;
-				} else if ((max2 == null || max2 < current) && current != max) {
-					max2 = current;
+	public List<String> longestLines(String fileName) throws IOException {  //LOCKED!
+		int currentMaxCount = 0;
+		List<String> lines = new ArrayList<String>();
+		try (BufferedReader br = new BufferedReader(
+				new InputStreamReader(
+						new FileInputStream(fileName)))) {
+			String line = null;
+			while ((line = br.readLine()) != null) {  //LOCKED!
+				int count = (line.split("\\s+")).length;
+				if (count > currentMaxCount) {
+					lines.clear();
+					lines.add(line);
+					currentMaxCount = count;
+				} else if (count == currentMaxCount) {
+					lines.add(line);
 				}
-
 			}
 		}
-		return max2;
+		return lines;
 	}
